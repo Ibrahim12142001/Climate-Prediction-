83d5f0afb4774b667734164ac7b450aa1fd41fd7
Gregory Parent <gparent@sfu.ca>
2024-11-08 11:31:05 -0800

    Added canada province pop data, just need to scale

--- Emissions/1-TransformData.py
+++ Emissions/1-TransformData.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 import os
+from shared import state_abbv_map, province_abbv_map
 
 def scale_population_data(population_data):
     # Our population data is interpolated for the year, and we want to use a common ratio across a year
@@ -22,6 +23,17 @@ def read_state_population_data(state_pop_file):
     pop_data = pop_data.astype(np.int64)
     return pop_data
 
+def read_province_population_data(province_pop_file):
+    pop_data = pd.read_csv(province_pop_file, parse_dates=[0])
+    pop_data = pop_data[pop_data['GEO'].isin(province_abbv_map.keys())] # keep only provinces we want
+    pop_data = pop_data[pop_data['REF_DATE'].dt.month == 10] # keep only Q4 data to match US scaling
+    pop_data = pop_data[['REF_DATE', 'GEO', 'VALUE']] # trim down to the columns we want
+    pop_data.columns = ['year', 'province', 'value'] # rename columns
+    pop_data['year'] = pop_data['year'].dt.year
+    pop_data = pop_data.sort_values(by=['province', 'year'])
+    return pop_data
+
+
 def interpolate_monthly(data, state_or_province):
     interpolated_data = []
     # Interpolation code is based on the yearly interpolation code for the population data
@@ -39,10 +51,10 @@ def interpolate_monthly(data, state_or_province):
         interpolated_data.append(group)
     return pd.concat(interpolated_data, ignore_index=True)
 
-def transform_to_us_cities(state_data, cities):
-    pass
+def transform_to_us_cities(state_data, cities_data, city_population_data, state_population_data):
+    print(state_data)
 
-def transform_to_canada_cities(province_data, cities):
+def transform_to_canada_cities(province_data, cities_data, city_population_data, province_population_data):
     pass
 
 def main():
@@ -65,11 +77,18 @@ def main():
 
     # Read in population files to be able to get ratios
     city_population_data = scale_population_data('../Population/Population_data.csv')
-    state_population_data = read_state_population_data('./state_pop_data_2000-2010.xls')
     # state population data is yearly taken from the US Census
     # source: https://www.census.gov/data/tables/time-series/demo/popest/intercensal-2000-2010-state.html
+    state_population_data = read_state_population_data('./state_pop_data_2000-2010.xls')
+    # province population data is yearly taken from Statistics Canada
+    # source: https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1710000901
+    province_population_data = read_province_population_data('./province_pop_data_2000-2010.csv')
 
     # Process each data file to convert them from State/Province emissions to estimated City emissions
+    us_city_emissions = transform_to_us_cities(interpolated_state_data, cities_data, city_population_data, state_population_data)
+    canada_city_emissions = transform_to_canada_cities(interpolated_province_data, cities_data, city_population_data, province_population_data)
+
+    # Merge city data and export it as our final step!
     
 
 
